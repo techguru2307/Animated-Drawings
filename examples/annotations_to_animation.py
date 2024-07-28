de0284c771fa2a04f6e47ee5119944244a59fe52
tech guru <techguru2307@gmail.com>
2024-07-28 21:31:58 +0530

    feat: created flask post api for generate mp4 file using animations.

---
+++ examples/annotations_to_animation.py
@@ -0,0 +1,106 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import animated_drawings.render
+import logging
+from pathlib import Path
+import sys
+import yaml
+from pkg_resources import resource_filename
+
+
+def annotations_to_animation(char_anno_dir: str, motion_cfg_fn: str, retarget_cfg_fn: str):
+    """
+    Given a path to a directory with character annotations, a motion configuration file, and a retarget configuration file,
+    creates an animation and saves it to {annotation_dir}/video.png
+    """
+
+    # package character_cfg_fn, motion_cfg_fn, and retarget_cfg_fn
+    animated_drawing_dict = {
+        'character_cfg': str(Path(char_anno_dir, 'char_cfg.yaml').resolve()),
+        'motion_cfg': str(Path(motion_cfg_fn).resolve()),
+        'retarget_cfg': str(Path(retarget_cfg_fn).resolve())
+    }
+
+    # create mvc config
+    mvc_cfg = {
+        'scene': {'ANIMATED_CHARACTERS': [animated_drawing_dict]},  # add the character to the scene
+        'controller': {
+            'MODE': 'video_render',  # 'video_render' or 'interactive'
+            'OUTPUT_VIDEO_PATH': str(Path(char_anno_dir, 'video.gif').resolve())}  # set the output location
+    }
+
+    # write the new mvc config file out
+    output_mvc_cfn_fn = str(Path(char_anno_dir, 'mvc_cfg.yaml'))
+    with open(output_mvc_cfn_fn, 'w') as f:
+        yaml.dump(dict(mvc_cfg), f)
+
+    # render the video
+    animated_drawings.render.start(output_mvc_cfn_fn)
+
+
+def custom_annotations_to_animation(char_anno_dir: str, motion_cfg_fn: str, retarget_cfg_fn: str, bg_image: str = None):
+    """
+    Given a path to a directory with character annotations, a motion configuration file, and a retarget configuration file,
+    creates an animation and saves it to {annotation_dir}/video.png
+    """
+
+    # package character_cfg_fn, motion_cfg_fn, and retarget_cfg_fn
+    animated_drawing_dict = {
+        'character_cfg': str(Path(char_anno_dir, 'char_cfg.yaml').resolve()),
+        'motion_cfg': str(Path(motion_cfg_fn).resolve()),
+        'retarget_cfg': str(Path(retarget_cfg_fn).resolve())
+    }
+
+    # create mvc config
+    mvc_cfg = {
+        'scene': {'ANIMATED_CHARACTERS': [animated_drawing_dict]},  # add the character to the scene
+        'controller': {
+            'MODE': 'video_render',  # 'video_render' or 'interactive'
+            'OUTPUT_VIDEO_PATH': str(Path(char_anno_dir, 'video.mp4').resolve())
+        }  # set the output location
+    }
+
+    view_dict = {}
+    if bg_image:
+        view_dict = {
+            "CAMERA_POS": [0.1, 1.3, 2.7],
+            "USE_MESA": True,
+            "WINDOW_DIMENSIONS": [300, 400],
+            "BACKGROUND_IMAGE": f"uploads/bg_images/{bg_image}",
+        }
+        mvc_cfg['view'] = view_dict
+
+    mvc_cfg['controller']['OUTPUT_VIDEO_CODEC'] = 'mp4v'
+    # view_dict['CAMERA_POS'] = [2.0, 0.7, 8.0]
+    # view_dict['CAMERA_FWD'] = [0.0, 0.5, 8.0]
+    mvc_cfg['view'] = view_dict
+    mvc_cfg['view']["USE_MESA"] = False
+
+    # write the new mvc config file out
+    output_mvc_cfn_fn = str(Path(char_anno_dir, 'mvc_cfg.yaml'))
+    with open(output_mvc_cfn_fn, 'w') as f:
+        yaml.dump(dict(mvc_cfg), f)
+
+    # render the video
+    animated_drawings.render.start(output_mvc_cfn_fn)
+
+
+if __name__ == '__main__':
+
+    log_dir = Path('./logs')
+    log_dir.mkdir(exist_ok=True, parents=True)
+    logging.basicConfig(filename=f'{log_dir}/log.txt', level=logging.DEBUG)
+
+    char_anno_dir = sys.argv[1]
+    if len(sys.argv) > 2:
+        motion_cfg_fn = sys.argv[2]
+    else:
+        motion_cfg_fn = resource_filename(__name__, 'config/motion/dab.yaml')
+    if len(sys.argv) > 3:
+        retarget_cfg_fn = sys.argv[3]
+    else:
+        retarget_cfg_fn = resource_filename(__name__, 'config/retarget/fair1_ppf.yaml')
+
+    annotations_to_animation(char_anno_dir, motion_cfg_fn, retarget_cfg_fn)
